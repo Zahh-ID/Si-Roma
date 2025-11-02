abf6f31dc46725d8a1bb5688bae577c826040471
Zahh-id <zahidpulo53@gmail.com>
2025-11-02 21:44:43 +0700

    push

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'new-welcome')->name('landing');
 
 Route::prefix('dashboard')->name('dashboard.')->group(function () {
-    Route::view('/', 'dashboard.index')->name('index');
-    Route::view('/katalog', 'dashboard.catalog')->name('catalog');
-    Route::view('/si-bot', 'dashboard.si-bot')->name('si-bot');
-    Route::view('/lacak-pesanan', 'dashboard.tracking')->name('track');
+    Route::view('/', 'user.dashboard.index')->name('index');
+    Route::view('/katalog', 'user.dashboard.catalog')->name('catalog');
+    Route::view('/si-bot', 'user.dashboard.si-bot')->name('si-bot');
+    Route::view('/lacak-pesanan', 'user.dashboard.tracking')->name('track');
 });
